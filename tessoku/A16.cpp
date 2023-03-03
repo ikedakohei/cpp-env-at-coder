5fbd08efa129b5409688f1ebb1c403ed365a9c2a
kohei <kouhei.ikeda@veriserve.co.jp>
2023-03-03 07:21:54 +0000

    鉄則 A16

--- tessoku/A16.cpp
+++ tessoku/A16.cpp
@@ -12,15 +12,15 @@ using mint = modint1000000007;
 int main() {
   int n;
   cin >> n;
-  vector<int> a(n - 1), b(n - 2), db(n);
+  vector<int> a(n - 1), b(n - 2), dp(n);
   rep(i, n - 1) cin >> a[i];
   rep(i, n - 2) cin >> b[i];
 
-  db[0] = 0;
-  db[1] = a[0];
+  dp[0] = 0;
+  dp[1] = a[0];
   for (int i = 2; i < n; i++) {
-    db[i] = min(db[i - 1] + a[i - 1], db[i - 2] + b[i - 2]);
+    dp[i] = min(dp[i - 1] + a[i - 1], dp[i - 2] + b[i - 2]);
   }
 
-  cout << db[n - 1] << endl;
+  cout << dp[n - 1] << endl;
 }
